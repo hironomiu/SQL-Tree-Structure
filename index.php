8b5d5d7d249e328cd0af2eadbaf67cb533c88998
hironomiu-vg <hironori_miura@voyagegroup.com>
2017-03-02 13:40:14 +0900

    インデント

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="/css/base.css">
+  <body>
+
 <?php
 
 echo "chapter2" . "<br>";
 
 ?>
 
-<a href="comments.php">comments.php</a><br>
-<a href="comments_22.php">隣接リスト</a><br>
-<a href="comments_251.php">経路列挙</a><br>
-<a href="comments_252.php">入れ子集合</a><br>
-<a href="comments_253.php">閉包テーブル</a><br>
+    <a href="comments.php">comments.php</a><br>
+    <a href="comments_22.php">隣接リスト</a><br>
+    <a href="comments_251.php">経路列挙</a><br>
+    <a href="comments_252.php">入れ子集合</a><br>
+    <a href="comments_253.php">閉包テーブル</a><br>
+
+  </body>
+</html>
